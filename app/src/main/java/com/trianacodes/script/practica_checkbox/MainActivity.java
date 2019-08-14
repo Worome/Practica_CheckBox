a80817d505e37c320b12817f2807d03df137aeaf
Worome <41673409+Worome@users.noreply.github.com>
2019-08-14 12:27:45 +0200

    Declaración de objetos

--- app/src/main/java/com/trianacodes/script/practica_checkbox/MainActivity.java
+++ app/src/main/java/com/trianacodes/script/practica_checkbox/MainActivity.java
@@ -2,12 +2,22 @@ package com.trianacodes.script.practica_checkbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_valor1, et_valor2;
+    private CheckBox ch_suma, ch_resta;
+    private TextView tv_resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
